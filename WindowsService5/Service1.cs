3531cf782e9a909ca6ced3e9d98b74da59ade4aa
Fatih İdris Geçgin <93212360+Fatihgcgn@users.noreply.github.com>
2024-02-27 16:13:28 +0300

    Add files via upload

---
+++ WindowsService5/Service1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService5
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            DosyaYaz("Servis calismaya basladi " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            DosyaYaz("Servis calismayi durdurdu " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object source,ElapsedEventArgs e)
+        {
+            DosyaYaz("Servis calismaya devam ediyor " + DateTime.Now);
+        }
+
+        public void DosyaYaz(string mesaj)
+        {
+            string dosyaYolu = AppDomain.CurrentDomain.BaseDirectory + "/Logs";
+
+            if(!Directory.Exists(dosyaYolu))
+            {
+                Directory.CreateDirectory(dosyaYolu);
+            }
+
+            string textYolu = AppDomain.CurrentDomain.BaseDirectory + "/Logs/servisim.txt";
+
+            if(!File.Exists(textYolu))
+            {
+                using (StreamWriter sw = File.CreateText(textYolu))
+                {
+                    sw.WriteLine(mesaj);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(textYolu))
+                {
+                    sw.WriteLine(mesaj);
+                }
+            }
+        }
+    }
+}
